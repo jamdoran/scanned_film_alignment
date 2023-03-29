6a90fe4b51af3edee7c2a6259665480376826846
jamdoran <jimmy@the-dorans.com>
2023-03-29 17:59:15 +0100

    Added installation instructions

--- setup.sh
+++ setup.sh
@@ -1,30 +1,54 @@
 # To run:   source setup.sh
 
-# Needed for wxPython on arm64 based Macs
-#arch_name="$(uname -m)"
-#if [ "${arch_name}" = "arm64" ]; then
-#    export CFLAGS=-I/$(brew --prefix)/include
-#    export CXXFLAGS=-I/$(brew --prefix)/include
-#    echo Setting Compiler flags for ARM wxPython build
-#else
-#    echo Not ARM - no compiler flags needed
-#fi
+# Install python-tk version 3.10 specifically
+brew install python-tk@3.10
 
-
-#Try to install python-tk via homebrew if not already installed
-tk_installed="$(brew info python-tk)"
-
-if [[ "$tk_installed" == *"Not installed"* ]]; then
-   brew install python-tk@3.9
-else
-   echo python-tk already brewed - moving right along...
-fi
-
-
-
-python3 -m venv .venv
+# make sure to use python3.10
+python3.10 -m venv .venv
 source .venv/bin/activate
 
+#Upgrade and install everything necessary
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
+
+
+
+
+
+Setup process on a clean Mac
+
+
+Install iTerm for sanity sake
+
+# Install xcode command line tools
+xcode-select --install
+    Wait for this to finish - it can take a while
+
+
+# Install HomeBrew as per instructions on home page
+# Add brew to the path
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/jamdoran/.zprofile
+
+
+#Install Python 10, zsh etc
+brew install python@3.10, zsh, python-tk@3.10, ffmpeg
+chsh -s /usr/local/bin/zsh
+
+#Open a new iTerm tab and check we have the correct zsh
+$ which zsh
+/usr/local/bin/zsh   //correct
+
+
+# Clone the repos into the Git Folder
+mkdir ~/Documents/Git
+cd ~/Documents/Git
+git clone https://github.com/jamdoran/scanned_film_alignment/
+git clone https://github.com/jamdoran/tiff_bulk_convert
+
+
+# Add alias to .zshrc
+alias convert='cd ~/Documents/Git/tiff_bulk_convert && source .venv/bin/activate && python tiff_convert.py'
+alias sprocket='cd ~/Documents/Git/scanned_film_alignment && source .venv/bin/activate && python scanned_film_alignment.py'
+
+#Should be good to go
 
